0b569e34a6fa63bc733cef805342ac48bb63f609
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2024-05-08 11:45:26 +0100

    Rename repo to tech-docs in preview docker
    We've changed the name of the repo, means we can also simplify what we
    tag the docker image with

--- preview-with-docker.sh
+++ preview-with-docker.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 set -e
 
-docker build . --tag authentication-tech-docs
+docker build . --tag tech-docs
 
-docker run -p 4567:4567 -p 35729:35729 -v $(pwd):/usr/src/docs -it authentication-tech-docs
+docker run -p 4567:4567 -p 35729:35729 -v $(pwd):/usr/src/docs -it tech-docs
