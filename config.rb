10e3ba5d4f35c67a858fb764b295b5fcdff49720
Craig Earl <craig.earl@digital.cabinet-office.gov.uk>
2025-12-01 14:03:04 +0000

    BAU: Lowering max concurrency to 1 for htmlproofer
    - This step has been a bit flaky recently. Lowering the concurrency will make the step take a bit longer but should make it more reliable.

--- config.rb
+++ config.rb
@@ -29,7 +29,7 @@ after_build do |builder|
         ],
         :swap_urls => { config[:tech_docs][:host] => "" },
         # reduce concurrency to avoid overwhelming external servers
-        hydra: { max_concurrency: 3 },
+        hydra: { max_concurrency: 1 },
         typhoeus: {
             # Some external links need to think you're in a browser to serve non-error codes
             headers: { "User-Agent" => "Mozilla/5.0 (Android 14; Mobile; LG-M255; rv:122.0) Gecko/122.0 Firefox/122.0" }
